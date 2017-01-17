da0338fa66f9368d6efbfc02ff1c6a43379f9dbe
Gerhard Messelink <gerhard.messelink@gmail.com>
2017-01-17 22:28:00 +0100

    database config for dev

--- holzstock/settings/base.py
+++ holzstock/settings/base.py
@@ -111,9 +111,9 @@ WSGI_APPLICATION = 'holzstock.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DATABASE_NAME', 'holzstock-dev'),
-        'USER': os.getenv('DATABASE_USER', 'holzstock-dev'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'holzstock-dev'),
+        'NAME': os.getenv('DATABASE_NAME', 'holzstock'),
+        'USER': os.getenv('DATABASE_USER', 'holzstock'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'holzstock'),
         'HOST': os.getenv('DATABASE_HOST', 'localhost'),
         'PORT': os.getenv('DATABASE_PORT', '5432'),
     }
