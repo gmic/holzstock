6c6fe250f7aa9dad0d0a20bf6c29c5040b15961f
Gerhard Messelink <gerhard.messelink@gmail.com>
2017-01-17 08:19:41 +0100

    develop

--- holzstock/wsgi.py
+++ holzstock/wsgi.py
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "holzstock.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "holzstock.settings.development")
 
 application = get_wsgi_application()
