0913480ba04fd983775338a522011e24b68a426d
Gerhard Messelink <gerhard.messelink@gmail.com>
2017-01-23 08:48:24 +0100

    Ansible not setting env properly

--- holzstock/settings/production.py
+++ holzstock/settings/production.py
@@ -6,3 +6,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['.holzstock.com']
 WWW_HOST = "www.holzstock.com"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME', 'holzstock'),
+        'USER': os.getenv('DATABASE_USER', 'holzstock'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'holzstock'),
+        'HOST': os.getenv('DATABASE_HOST', 'localhost'),
+        'PORT': os.getenv('DATABASE_PORT', '5432'),
+    }
+}
