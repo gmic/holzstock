d0eb170fec55ae7247e93b023b0f22ab7b4c397b
Gerhard Messelink <gerhard.messelink@gmail.com>
2017-01-23 08:41:21 +0100

    Top logo

--- holzstock/settings/base.py
+++ holzstock/settings/base.py
@@ -107,9 +107,9 @@ WSGI_APPLICATION = 'holzstock.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DATABASE_NAME', 'holzstock'),
-        'USER': os.getenv('DATABASE_USER', 'holzstock'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'holzstock'),
+        'NAME': os.getenv('DATABASE_NAME', 'holzstock-dev'),
+        'USER': os.getenv('DATABASE_USER', 'holzstock-dev'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', 'holzstock-dev'),
         'HOST': os.getenv('DATABASE_HOST', 'localhost'),
         'PORT': os.getenv('DATABASE_PORT', '5432'),
     }
