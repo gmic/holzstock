1f11961803203e614663b4a2d485ef760823916b
Gerhard Messelink <gerhard.messelink@gmail.com>
2017-01-24 07:46:51 +0100

    max width 992px

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # holzstock
 holzstock.com website
+
+
+# Running gulp
+
+cd holzstock/static/base_theme
+npm install
+gulp
--- home/models.py
+++ home/models.py
@@ -16,6 +16,22 @@ COLOUR_CHOICES = [
 ]
 
 
+class OneColumnBlock(blocks.StructBlock):
+    background = blocks.ChoiceBlock(choices=COLOUR_CHOICES, default="bg-white")
+    center_column = blocks.StreamBlock([
+        ('heading', blocks.CharBlock(classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('embedded_video', EmbedBlock()),
+        # ('google_map', GoogleMapBlock()),
+    ], icon='user', label='Center column')
+
+    class Meta:
+        template = 'home/blocks/one_column_block.html'
+        icon = 'placeholder'
+        label = 'One Column'
+
+
 class TwoColumnBlock(blocks.StructBlock):
     background = blocks.ChoiceBlock(choices=COLOUR_CHOICES, default="bg-white")
     left_column = blocks.StreamBlock([
@@ -40,6 +56,38 @@ class TwoColumnBlock(blocks.StructBlock):
         label = 'Two Columns'
 
 
+class ThreeColumnBlock(blocks.StructBlock):
+    background = blocks.ChoiceBlock(choices=COLOUR_CHOICES, default="bg-white")
+    left_column = blocks.StreamBlock([
+        ('heading', blocks.CharBlock(classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('embedded_video', EmbedBlock()),
+        # ('google_map', GoogleMapBlock()),
+    ], icon='arrow-left', label='Left column content')
+
+    column = blocks.StreamBlock([
+        ('heading', blocks.CharBlock(classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('embedded_video', EmbedBlock()),
+        # ('google_map', GoogleMapBlock()),
+    ], icon='user', label='Center column')
+
+    right_column = blocks.StreamBlock([
+        ('heading', blocks.CharBlock(classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('embedded_video', EmbedBlock()),
+        # ('google_map', GoogleMapBlock()),
+    ], icon='arrow-right', label='Right column content')
+
+    class Meta:
+        template = 'home/blocks/three_column_block.html'
+        icon = 'placeholder'
+        label = 'Three Columns'
+
+
 class JumbotronBlock(blocks.StructBlock):
     image = ImageChooserBlock()
     body = blocks.RichTextBlock()
@@ -51,7 +99,9 @@ class JumbotronBlock(blocks.StructBlock):
 
 class HomePage(Page):
     body = StreamField([
+        ('one_column', OneColumnBlock()),
         ('two_columns', TwoColumnBlock()),
+        ('three_columns', ThreeColumnBlock()),
         ('jumbotron', JumbotronBlock()),
     ])
 
